83a4545ee17fb02900384303c2cd0e2f4511effa
Paul Giraudon <paul.giraudon@gmail.com>
2019-03-14 15:29:27 +0100

    Sample using WebSocket

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ var proxy = require('http-proxy-middleware')
 
 var app = express()
 
-app.use('/user-api', proxy({ target: 'http://jpro1-macpagnol.jamespot.pro/user-api', changeOrigin: true }))
+
+var socketProxy = proxy('ws://jpro1-macpagnol.jamespot.pro/socket.io', { changeOrigin: true })
+app.use('/socket.io', socketProxy)
+
+app.use('/reactTest', proxy({ target: 'http://jpro1-macpagnol.jamespot.pro', changeOrigin: true }))
+app.use('/user-api', proxy({ target: 'http://jpro1-macpagnol.jamespot.pro', changeOrigin: true }))
+
 app.use('/', proxy({ target: 'http://localhost:3000', changeOrigin: true }))
 
-app.listen(3333)
+
+
+var server = app.listen(3333);
+
+server.on('upgrade', socketProxy.upgrade);
