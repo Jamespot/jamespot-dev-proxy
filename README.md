2026807a4269d72db15788e7f1091ff987579dfe
Paul Giraudon <paul.giraudon@jamespot.com>
2020-06-05 17:23:49 +0200

    Readme updated

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 # jamespot-dev-proxy
 
-A sample dev project, to developp a ReactStyle projet, using a Jamespot Pro platform as a backend.
+A sample proxy project, to developp a ReactStyle projet, 
+using a Jamespot Pro platform as a backend, 
+and node to serve React compiled JS during developpement.
 
+The proxy should be deployed locally, (say 127.0.0.1),
+and adressed with the local name : jamespot.proxy.
+
+
+e.g in /etc/hosts :
+
+---
+
+127.0.0.1  jamespot.proxy
+
+---
+
+
+Then, you should access your proxy using : https://jamespot.proxy:3443/  
+
+In order to suppress Certificates Warning, 
+the Certificate Store must contain the XCA_Jamespot.crt certificate as a Certificate Authority.
--- index.js
+++ index.js
@@ -9,36 +9,31 @@ var proxy = require('http-proxy-middleware')
 var app = express()
 
 https.createServer({
-    key: fs.readFileSync('js.teamkline.kline.app.key'),
-    cert: fs.readFileSync('js.teamkline.kline.app.crt')
+    key: fs.readFileSync('jamespot.proxy.key'),
+    cert: fs.readFileSync('jamespot.proxy.crt')
   }, app).listen(3443);
    
 
 var domain = "k2018-1.jamespot.pro";
+
 var protocol = 'https';
 
-var socketProxy = proxy('ws://'+domain+'/socket.io', { changeOrigin: true })
-app.use('/socket.io', socketProxy)
 
 
 var proxyOptions = { 
   target: protocol+'://'+domain, 
-  cookieDomainRewrite: ".teamkline.kline.app",
+  cookieDomainRewrite: ".jamespot.proxy",
   changeOrigin: true,
   headers: {Referer: 'https://'+domain+'/'}
 }
 
+var socketProxy = proxy( 'wss://'+domain+'/socket.io', {changeOrigin: true , headers: {Referer: 'https://'+domain+'/'}})
+app.use('/socket.io', socketProxy)
 
-app.use('/reactTest', proxy(proxyOptions))
-app.use('/user-api', proxy(proxyOptions))
-app.use('/image-cache', proxy(proxyOptions))
+
+app.use('/reactTest', proxy({ target: 'http://localhost:3000', changeOrigin: true }))
 app.use('/', proxy(proxyOptions))
 
-
-/*
-app.use('/', proxy({ target: 'http://localhost:3000', changeOrigin: true }))
-app.use('/', proxy({ target: 'http://localhost:3000', changeOrigin: true }))
-*/
 
 var server = app.listen(3333);
 
